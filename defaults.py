ccf8e9180d05c0ae49cf641ff11d0f63e3a27c65
Lemuel <lemuelkumarga@gmail.com>
2018-04-12 13:07:45 +0800

    Add cache functionality

--- defaults.py
+++ defaults.py
@@ -259,3 +259,27 @@ py_config = dict(
                               'toggleHover',
                               'sendDataToCloud']
 )
+
+""" =================================
+    Caching Functions
+================================= """
+load({"pck":"pickle",
+      "os":"os"})
+
+__cache_folder = "cache/"
+
+def cache(fun, name, override=False):
+	
+	filename = __cache_folder + name + ".pkl"
+	
+	if (os.path.isfile(filename)):
+	    with open(filename, 'rb') as f:
+	        tmp = pck.load(f)
+	        f.close()
+	else:
+	    tmp = fun()
+	    with open(filename, 'wb') as f:
+	        pck.dump(tmp, f, pck.HIGHEST_PROTOCOL)
+	        f.close()
+	
+	return tmp
